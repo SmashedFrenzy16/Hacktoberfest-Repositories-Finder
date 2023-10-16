e482150e8abec9d8600985793912245f6ec6597d
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-10-16 20:42:33 +0100

    Update hacktoberfest_search.py with code

--- hacktoberfest_search.py
+++ hacktoberfest_search.py
@@ -1,1 +1,19 @@
+from github import Github
 
+g_cred = Github("<Paste Your Token Here>")
+
+count = int(input("Enter in the number of repositories to display: "))
+
+repo_count = count
+
+icount = 0
+
+for i in g_cred.search_repositories("topic:hacktoberfest"):
+
+    print(f"{icount + 1}. {i}")
+
+    icount += 1
+
+    if icount >= repo_count:
+
+        break
